90b11a7e98ec63223c5b0c1a3d04bf02de9a97ea
JL Granda <jorge_granda82112@elpoli.edu.co>
2018-03-22 20:31:47 -0500

    TODO

--- config.js
+++ config.js
@@ -5,6 +5,9 @@
 
 module.exports= {
   port: process.env.PORT || 3000,
-  db: process.env.MONGODB || 'mongodb://localhost:27017/shop',
+  db: process.env.MONGODB || 'mongodb://david:lO4b4oYOasO5X3eX@cluster0-shard-00-00-xfpif.mongodb.net:27017,'+
+                              'cluster0-shard-00-01-xfpif.mongodb.net:27017,' +
+                              'cluster0-shard-00-02-xfpif.mongodb.net:27017' +
+                              '/shop?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
   SECRET_TOKEN: 'miclavedetokens'
 };
